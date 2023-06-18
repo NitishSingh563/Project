e3e803647ecf53aaca4876aa6b3d8d5062076235
Nitish Singh <123872258+NitishSingh563@users.noreply.github.com>
2023-06-18 13:45:14 +0530

    Digital Clock

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
-const form = document.querySelector('form')
+const clock = document.querySelector('#clock');
 
-form.addEventListener('submit', function(e){
-     e.preventDefault();
 
-  const height =  parseInt(document.querySelector('#height').value);
-  const weight =  parseInt(document.querySelector('#weight').value);
-  const results = document.querySelector('#results');
 
-  if (height === '' || height < 0 || isNaN(height)) {
-     results.innerHTML = `Please give a valid Height ${height}`
-} else if (weight === '' || height < 0 || isNaN(weight)) {
-     results.innerHTML = `Please give a valid weight ${weight}`
-} else { 
-    const bmi = (weight / ((height * height)/10000)).toFixed(2)
-    results.innerHTML = `<span> ${bmi} </span>`;
-}
-     
-});
+setInterval(function(){
+     let date = new Date()
+     clock.innerHTML =  date.toLocaleTimeString();
+},1000)
