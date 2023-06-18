3379265a09886dea65235504bd666c333b712223
Nitish Singh <123872258+NitishSingh563@users.noreply.github.com>
2023-06-18 13:47:05 +0530

    color changer

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-const clock = document.querySelector('#clock');
+const buttons = document.querySelectorAll ('.button');
+const body = document.querySelector("body");
 
-
-
-setInterval(function(){
-     let date = new Date()
-     clock.innerHTML =  date.toLocaleTimeString();
-},1000)
+buttons.forEach(function(button) {
+     button.addEventListener('click', function(e){
+          if (e.target.id === 'red') {
+            body.style.backgroundColor = e.target.id;   
+          }
+          if (e.target.id === 'Green') {
+            body.style.backgroundColor = e.target.id;   
+          }
+          if (e.target.id === 'Orange') {
+            body.style.backgroundColor = e.target.id;   
+          }
+          if (e.target.id === 'Yellow') {
+            body.style.backgroundColor = e.target.id;   
+          }
+          if (e.target.id === 'Purple') {
+            body.style.backgroundColor = e.target.id;   
+          }
+          if (e.target.id === 'Brown') {
+            body.style.backgroundColor = e.target.id;   
+          }
+     });
+});
